bf63a464c69a002fc59c9cc1f04eb68f5a0a4d58
Daniel Britto <daniel.britto.jannuss@premexcorp.com>
2026-03-28 13:07:45 -0500

    cambio de validacion para que funcione con mastwr y no con main

--- .husky/check-branch.sh
+++ .husky/check-branch.sh
@@ -5,11 +5,11 @@ LC_ALL=C
 local_branch_name="$(git rev-parse --abbrev-ref HEAD)"
 
 # Expresión regular para nombres válidos (puedes ajustarla a tu gusto)
-valid_branch_regex="^(feature|fix|bugfix|hotfix|chore|refactor|docs|style|test|perf|ci|build|revert|experimental)\/[a-z0-9._-]+$|^(main|dev|qa)$"
+valid_branch_regex="^(feature|fix|bugfix|hotfix|chore|refactor|docs|style|test|perf|ci|build|revert|experimental)\/[a-z0-9._-]+$|^(master|dev|qa)$"
 
 if [[ ! $local_branch_name =~ $valid_branch_regex ]]; then
     echo "❌ ERROR: Nombre de rama inválido: '$local_branch_name'"
-    echo "Formatos permitidos: feature/, fix/, hotfix/, chore/, docs/ o main/dev/qa"
+    echo "Formatos permitidos: feature/, fix/, hotfix/, chore/, docs/ o master/dev/qa"
     exit 1
 fi
 
